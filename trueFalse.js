b3e33edeb0d0a20ebd6d0ea720349e7b72d22e94
shahriar jalal <shahjalalsbc@gmail.com>
2021-02-02 16:38:41 +0600

    javascript truthy and falsy logic

---
+++ trueFalse.js
@@ -0,0 +1,34 @@
+const age = 10;
+// const age = 0;
+if(age) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+//for string
+// const fName = "jalal";
+const fName = " ";
+if(fName) {
+    console.log("condition is true");
+}
+ else {
+    console.log("condition is false");
+}
+//
+let empty;
+// let empty=null;
+
+if(empty) {
+    console.log(empty);
+} else {
+    console.log(!empty);
+}
+
+//number logic comparision
+// let number;
+let number=0;
+if(number || number == 0) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
