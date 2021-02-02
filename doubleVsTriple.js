6fa8691e09e0e02e1ec378e17c781f650ee228d5
shahriar jalal <shahjalalsbc@gmail.com>
2021-02-02 17:44:35 +0600

    double vs triple equal

---
+++ doubleVsTriple.js
@@ -0,0 +1,24 @@
+
+const first = 2;
+const sec = 5;
+
+if(first == sec ) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+if(first === sec ) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+const fValue = 0;
+const sValue = false;
+
+if(fValue == sValue) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
