4ee7847114f4ece1d646b54724019f0e66d5584d
shahriar jalal <shahjalalsbc@gmail.com>
2021-02-02 17:03:27 +0600

    null vs undefined

---
+++ nullVsUndefined.js
@@ -0,0 +1,22 @@
+let empty;
+
+function add(num1,num2) {
+    console.log(num1 + num2);
+}
+const result = add(20,30);
+console.log(result);
+
+//object
+
+const fulladdress = {
+    name : "jalal",
+    id : 15,
+}
+console.log(fulladdress.phone);
+
+let fun = undefined;
+console.log(fun);
+
+// array undefined
+const nums = [2,5,3];
+console.log(nums[5]);
