6a3339df3ce933db10ec0831abc87eedb6c42aaa
shahriar jalal <shahjalalsbc@gmail.com>
2021-02-03 13:01:22 +0600

    array split

---
+++ arraySplit.js
@@ -0,0 +1,20 @@
+const nums = [6,5,3,8,7,16,18,9];
+
+
+const slicePart = nums.slice(2,5);
+const splicePart = nums.splice(2,5);
+const together = nums.join(" : ");
+
+console.log("slicePart\n");
+console.log("slicePart", slicePart);
+console.log("nums", nums);
+
+
+console.log("splicePart:");
+console.log("splicePart", splicePart);
+console.log("nums", nums);
+console.log("together", together);
+
+
+
+
